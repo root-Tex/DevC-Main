62226cc68034688731bf875e062ad11846894abc
root-Tex <175974114+root-Tex@users.noreply.github.com>
2024-10-09 10:27:42 -0400

    Se actualizo el archivo main, tiene 10 lineas nuevas

--- main.cpp
+++ main.cpp
@@ -2,10 +2,20 @@
 using namespace std;
 int main()
 {
+	cout<<"ACTUALIZACION";
+
 	//El programa esta terminado hasta este momento
 	cout<<"Hello World";
 	cout<<"Program finished";
 	
-	count<<"this is a change the module"
+	count<<"this is a change the module";
+
+	cout<<"ACTUALIZACION";
+	cout<<"ACTUALIZACION";
+	cout<<"ACTUALIZACION";
+	cout<<"ACTUALIZACION";
+	cout<<"ACTUALIZACION";
+
+	cout<<"ACTUALIZACION"; cout<<"ACTUALIZACION"; cout<<"ACTUALIZACION";
 	
 }
